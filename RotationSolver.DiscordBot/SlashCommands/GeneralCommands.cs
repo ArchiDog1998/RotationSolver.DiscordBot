e832fdf687c1c6944c0ad6867fb06098a4eea890
秋水 <1123993881@qq.com>
2024-06-17 10:10:11 +0800

    fix: add thread member in that channel.

--- RotationSolver.DiscordBot/SlashCommands/GeneralCommands.cs
+++ RotationSolver.DiscordBot/SlashCommands/GeneralCommands.cs
@@ -234,6 +234,8 @@ public class GeneralCommands : ApplicationCommandModule
             .WithMessage(message)
             .WithName(tittle));
 
+        await forum.Channel.AddThreadMemberAsync(ctx.Member);
+
         await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Created a new post " + forum.Channel.Mention));
     }
 }
